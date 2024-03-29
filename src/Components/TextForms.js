e0bd3da5cb7a0a0289614ea11fdb20298c550330
TamimOp <tamimshadanik@gmail.com>
2024-03-30 03:13:51 +0600

    add and changes files

---
+++ src/Components/TextForms.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+export default function TextForms(props) {
+  const handleUpClick = () => {
+    console.log("Uppercase was Clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLoClick = () => {
+    console.log("Uppercase was Clicked");
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleOnChange = (e) => {
+    console.log("on change");
+    setText(e.target.value);
+  };
+  const [text, setText] = useState("Enter Text");
+  return (
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+      </div>
+      <div className="container my-3">
+        <h1>Your Text Summery</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} charecter
+        </p>
+      </div>
+    </>
+  );
+}
